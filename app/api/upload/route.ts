d8d2c439e9b46a412118a2aef27204869cfbbe2e
Onyekachi James <okilonet1@gmail.com>
2023-06-28 21:50:02 +0100

    push

---
+++ app/api/upload/route.ts
@@ -0,0 +1,130 @@
+import { NextResponse } from "next/server";
+import { google } from "googleapis";
+import { Readable, PassThrough } from "stream";
+import prisma from "@/lib/prismadb";
+
+const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const drive = google.drive({
+  version: "v3",
+  auth: oauth2Client,
+});
+
+const folderId = process.env.NEXT_PUBLIC_GOOGLE_DRIVE_FOLDER_ID!;
+
+async function generatePublicUrl(id: string) {
+  try {
+    await drive.permissions.create({
+      fileId: id,
+      requestBody: {
+        role: "reader",
+        type: "anyone",
+      },
+    });
+
+    const result = await drive.files.get({
+      fileId: id,
+      fields: "webViewLink, webContentLink",
+    });
+
+    return result.data as { webViewLink: string; webContentLink: string };
+  } catch (error: any) {
+    console.log(error.message);
+  }
+}
+
+async function uploadFile(file: any) {
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+
+  const readableStream = new Readable();
+  readableStream.push(buffer);
+  readableStream.push(null);
+
+  try {
+    const response = await drive.files.create({
+      requestBody: {
+        parents: [folderId],
+        name: file.name,
+      },
+      media: {
+        mimeType: file.type,
+        body: readableStream,
+      },
+    });
+
+    return response.data as {
+      kind: string;
+      id: string;
+      name: string;
+      mimeType: string;
+    };
+  } catch (error: any) {
+    console.log(error.message);
+  }
+}
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+  const file = formData.get("file") as File;
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const phone = formData.get("phone") as string;
+
+  if (file.length < 1 || !name || !email || !phone) {
+    return new NextResponse("Invalid Credentials", {
+      status: 400,
+    });
+  }
+
+  const alreadyFilled = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  let user;
+
+  console.log(alreadyFilled, "alreadyFilled");
+
+  if (!alreadyFilled) {
+    try {
+      const fileUpload = await uploadFile(file);
+
+      const publicUrl = await generatePublicUrl(fileUpload.id);
+
+      user = await prisma.user.create({
+        data: {
+          name: name,
+          email: email,
+          phone: phone,
+          previewResume: publicUrl?.webViewLink!,
+          downloadResume: publicUrl?.webContentLink!,
+        },
+      });
+
+      return NextResponse.json(user);
+    } catch (error) {
+      console.log(error);
+      return new NextResponse("Error", {
+        status: 400,
+      });
+    }
+  } else {
+    return new NextResponse("You have already submitted before", {
+      status: 400,
+    });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,181 @@
-import Image from 'next/image'
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { InstagramEmbed } from "react-social-media-embed";
+import axios from "axios";
+import { use, useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { toast } from "react-hot-toast";
+import { ClipLoader } from "react-spinners";
+
+const formSchema = z.object({
+  name: z.string().min(3).max(255),
+  email: z.string().email(),
+  phone: z.string().min(10).max(15),
+  file: z.any(),
+});
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+      file: null,
+    },
+  });
+
+  if (isLoading === true) {
+    toast.loading("Uploading", {
+      id: "loading",
+    });
+  } else {
+    toast.remove("loading");
+  }
+
+  console.log(isLoading);
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    const data = form.getValues();
+
+    const formData = new FormData();
+
+    if (data.file == null) {
+      toast.error("Please Upload a file");
+    } else {
+      formData.append("file", data.file[0]);
+      formData.append("name", data.name);
+      formData.append("email", data.email);
+      formData.append("phone", data.phone);
+
+      setIsLoading(true);
+
+      axios
+        .post("/api/upload", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((res) => {
+          toast.success("Your Resume has been submitted successfully");
+          form.reset();
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+          toast.error(err.response.data || "An error occured");
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-5 p-2 max-w-lg mx-auto"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Full Name</FormLabel>
+              <FormControl>
+                <Input placeholder="John Doe" {...field} />
+              </FormControl>
+              <FormDescription>
+                Your Full Name as it appears on your certificate
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-      </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="johndoe@nuesaabuad.com" {...field} />
+              </FormControl>
+              <FormDescription>Your active email address</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone</FormLabel>
+              <FormControl>
+                <Input placeholder="+234 7057395799" {...field} />
+              </FormControl>
+              <FormDescription>Your active phone number</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Resume/CV</FormLabel>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <Input
+                type="file"
+                accept="application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                {...form.register("file")}
+              />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+              <FormDescription>
+                Your most recent Resume/CV (PDF)
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <Button type="submit" disabled={isLoading}>
+          Submit
+        </Button>
+
+        <Dialog>
+          <DialogTrigger>Tips</DialogTrigger>
+          <DialogContent>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <InstagramEmbed
+                url="https://www.instagram.com/reel/CthLRw3NlNR/?igshid=MzRlODBiNWFlZA=="
+                width={328}
+              />
+            </div>
+          </DialogContent>
+        </Dialog>
+      </form>
+    </Form>
+  );
 }
