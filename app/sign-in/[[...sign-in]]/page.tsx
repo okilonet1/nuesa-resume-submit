adc4c588d43e61fc04bdb519449d36c43e8f76e2
Onyekachi James <okilonet1@gmail.com>
2023-06-30 13:27:03 +0100

    styling

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <SignIn
+      appearance={{
+        elements: {
+          rootBox: "mx-auto my-8 w-full max-w-md ",
+        },
+      }}
+    />
+  );
 }
